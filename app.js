194342070b47d0bcc769b7992cc949fcb19bf4cb
Cryptonean Labs <81574358+CryptoneanLabs@users.noreply.github.com>
2022-07-29 18:19:33 +0800

    Update app.js
    bug fixes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 
 var app = express();
 var port = process.env.PORT || 7000;
-var baseDir ='http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl';
+var baseDir ='https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl';
 
 // cors config
 var whitelist = [
@@ -151,21 +151,22 @@ function getGribData(targetMoment){
         }
 
 		var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
+		var urlstamp = stamp.slice(0,8)+'/'+stamp.slice(8,10)+'/atmos';
 		request.get({
 			url: baseDir,
 			qs: {
-				file: 'gfs.t'+ roundHours(moment(targetMoment).hour(), 6) +'z.pgrb2.1p00.f000',
-				lev_10_m_above_ground: 'on',
-				lev_surface: 'on',
-				var_TMP: 'on',
-				var_UGRD: 'on',
-				var_VGRD: 'on',
-				leftlon: 0,
-				rightlon: 360,
-				toplat: 90,
-				bottomlat: -90,
-				dir: '/gfs.'+stamp
-			}
+			  file: 'gfs.t' + roundHours(moment(targetMoment).hour(), 6) + 'z.pgrb2.1p00.f000',
+			  lev_10_m_above_ground: 'on',
+			  lev_surface: 'on',
+			  var_TMP: 'on',
+			  var_UGRD: 'on',
+			  var_VGRD: 'on',
+			  leftlon: 0,
+			  rightlon: 360,
+			  toplat: 90,
+			  bottomlat: -90,
+			  dir: '/gfs.' + urlstamp,
+			},
 
 		}).on('error', function(err){
 			// console.log(err);
@@ -217,7 +218,7 @@ function convertGribToJson(stamp, targetMoment){
 
 	var exec = require('child_process').exec, child;
 
-	child = exec('converter/bin/grib2json --data --output json-data/'+stamp+'.json --names --compact grib-data/'+stamp+'.f000',
+	child = exec('./converter/bin/grib2json --data --output json-data/'+stamp+'.json --names --compact grib-data/'+stamp+'.f000',
 		{maxBuffer: 500*1024},
 		function (error, stdout, stderr){
 
